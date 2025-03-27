b3da52d408f70767f1853b7382e81ce2879000f9
wetooa <derpykidyt@gmail.com>
2025-03-27 23:14:30 +0800

    testing shadcn initialization

--- app/(public)/landing/page.tsx
+++ app/(public)/landing/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      LandingPage <Button>This is a button</Button>
+    </div>
+  );
 }
