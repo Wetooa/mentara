81769a4856e029c8c25dc30857f6936e53143f38
tr-ggr <tristanjamestolentino56@gmail.com>
2025-05-16 10:52:29 +0800

    fixed shit

--- mentara-client/app/(public)/(therapist)/therapist_signup/page.tsx
+++ mentara-client/app/(public)/(therapist)/therapist_signup/page.tsx
@@ -19,13 +19,6 @@ import { Input } from "@/components/ui/input";
 import PROVIDER_TYPE from "@/constants/provider";
 import PHILIPPINE_PROVINCES from "@/constants/provinces";
 
-// Import constants
-<<<<<<< HEAD
-import PHILIPPINE_PROVINCES from "@/constants/provinces";
-import PROVIDER_TYPE from "@/constants/provider";
-=======
->>>>>>> 22d5bbfd945123b82a3e609a6e137e4ebd17613e
-
 // Component interfaces
 interface FormInputProps {
   PlaceHolder: string;
