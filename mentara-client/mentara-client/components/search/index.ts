51e2f9276ae0e3d62a221f05b353b0ef897e6660
tr-ggr <tristanjamestolentino56@gmail.com>
2025-07-20 19:06:44 +0800

    Feature: Remove deprecated end-to-end tests and helper functions related to therapist recommendations and user onboarding flow

--- mentara-client/mentara-client/components/search/index.ts
+++
@@ -1,5 +0,0 @@
-export { UserSearchBar } from './UserSearchBar';
-export { UserSearchItem } from './UserSearchItem';
-export { UserSearchResults } from './UserSearchResults';
-export { useUserSearch } from './hooks/useUserSearch';
-export type { User } from './UserSearchItem';
